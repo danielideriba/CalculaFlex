e56b1abc377543020c9b3ff90e47d6eb806992f9
Logon Aluno <logonrmlocal@fiap.com.br>
2018-11-29 23:04:53 -0200

    Initial commit

---
+++ app/src/main/java/br/com/app/calculaflex/calculaflex/MyApplication.kt
@@ -0,0 +1,11 @@
+package br.com.app.calculaflex.calculaflex
+
+import com.facebook.stetho.Stetho
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initializeWithDefaults(this)
+    }
+}
---
+++ app/src/main/java/br/com/app/calculaflex/calculaflex/model/User.kt
@@ -0,0 +1,7 @@
+package br.com.app.calculaflex.calculaflex.model
+
+data class User(
+        val nome: String = "",
+        val email: String = "",
+        val fone: String = ""
+)
